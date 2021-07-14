86e96f04658dbc44e434532764a219501cc27385
Murat Çakmak <muratcakmak5252@gmail.com>
2021-07-14 21:14:12 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * If folderName have this characters \,/,:,*,?,",<,>,| return false but if not return true.
+ * @param $fileName
+ * @return string
+ */
+function folderOrFileNameRegex($fileName){
+	return !preg_match('/[\\\\,\/,:,*,?,",<,>,|]/', $fileName);
+}
+
+var_dump(folderOrFileNameRegex('test1\\')); // false
+var_dump(folderOrFileNameRegex('tes/t1')); // false
+var_dump(folderOrFileNameRegex('<test1')); // false
+var_dump(folderOrFileNameRegex('test')); // true
